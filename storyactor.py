b008f19126744b880b5c1f8a49f7c9a42cf384d7
Jerry Liu <jbliu52@gmail.com>
2025-09-22 12:12:38 -0400

    Create storyactor.py

---
+++ storyactor.py
@@ -0,0 +1,54 @@
+import random
+from mmap import ACCESS_COPY
+
+
+class Actor:
+
+    def __init__(self, name, actors):
+        self.name = name
+        self.trust = {}
+        self.maxTrust = 20
+        self.minTrust = 0
+        self.aidReward = 2
+        self.aidPenalty = -2
+        for actor in actors:
+            self.trust[actor] = (self.maxTrust + self.minTrust) / 2
+
+    def set_actors(self, actors):
+        for actor in actors:
+            self.trust[actor] = (self.maxTrust + self.minTrust) / 2
+
+    def request(self, actor):
+        """  """
+        if actor.aid(self):
+            self.trust[actor] += self.aidReward
+        else:
+            self.trust[actor] += self.aidPenalty
+
+    def aid(self, actor):
+        """ Randomly chooses to aid based on actor's trust level """
+        return True
+
+    def __repr__(self):
+        return self.name
+
+
+class SelfishActor(Actor):
+
+    def __init__(self, name, actors):
+        super().__init__(name, actors)
+
+    def aid(self, actor):
+        """ Never chooses to aid """
+        return False
+
+
+class SelflessActor(Actor):
+
+    def __init__(self, name, actors):
+        super().__init__(name, actors)
+
+    def aid(self, actor):
+        """ Always chooses to aid """
+        return
+
